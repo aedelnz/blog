1dc2e303e8e386dab8a6c2cf697ae357c3366447
麗姫を描く <aedelnz@qq.com>
2026-02-05 17:51:20 +0800

    Optimize adding an adaptive header

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = '博客';
+export const SITE_TITLE = 'Draw Ofs 博客';
 export const SITE_DESCRIPTION = '一条咸鱼躺平养成计划！';
